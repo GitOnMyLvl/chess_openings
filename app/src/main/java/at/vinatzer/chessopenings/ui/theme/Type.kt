be472cc2eedaebcc80bbfee4e9847ff043e69051
GitOnMyLvl <wolfgang.vinatzer@gmail.com>
2023-12-12 14:03:17 +0100

    Added title, pictures and notation

--- app/src/main/java/at/vinatzer/chessopenings/ui/theme/Type.kt
+++ app/src/main/java/at/vinatzer/chessopenings/ui/theme/Type.kt
@@ -2,33 +2,41 @@ package at.vinatzer.chessopenings.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import at.vinatzer.chessopenings.R
 
-// Set of Material typography styles to start with
+val Roboto = FontFamily(
+    Font(R.font.roboto_regular),
+    Font(R.font.roboto_bold, FontWeight.Bold)
+    )
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = Roboto,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 36.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayMedium = TextStyle(
+          fontFamily = Roboto,
+          fontWeight = FontWeight.Bold,
+          fontSize = 20.sp
+     ),
+    displaySmall = TextStyle(
+          fontFamily = Roboto,
+          fontWeight = FontWeight.Bold,
+          fontSize = 14.sp
+     ),
+    bodyLarge = TextStyle(
+          fontFamily = Roboto,
+          fontWeight = FontWeight.Normal,
+          fontSize = 14.sp
+     ),
+    bodyMedium = TextStyle(
+          fontFamily = Roboto,
+          fontWeight = FontWeight.Normal,
+          fontSize = 20.sp
+     )
 )
